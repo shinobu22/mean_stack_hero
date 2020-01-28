a281e5509faae943ca0f3aea4947930d63917fe1
iBlurBlur <tanakorn.ngam@gmail.com>
2020-01-28 12:37:59 +0700

    init stock component and set up routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/member/login/login.component';
 import { RegisterComponent } from './components/member/register/register.component';
+import { StockHomeComponent } from './components/stock/stock-home/stock-home.component';
+import { StockCreateComponent } from './components/stock/stock-create/stock-create.component';
+import { StockEditComponent } from './components/stock/stock-edit/stock-edit.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'stock', component: StockHomeComponent},
+  {path: 'stock/create', component: StockCreateComponent},
+  {path: 'stock/edit', component: StockEditComponent},
   {path: '**', redirectTo: 'login'}
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { HeaderComponent } from './components/shared/header/header.component';
 import { MenuComponent } from './components/shared/menu/menu.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { AuthService } from './services/auth.service';
+import { StockHomeComponent } from './components/stock/stock-home/stock-home.component';
+import { StockCreateComponent } from './components/stock/stock-create/stock-create.component';
+import { StockEditComponent } from './components/stock/stock-edit/stock-edit.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,9 @@ import { AuthService } from './services/auth.service';
     HeaderComponent,
     MenuComponent,
     FooterComponent,
+    StockHomeComponent,
+    StockCreateComponent,
+    StockEditComponent,
   ],
   imports: [
     BrowserModule,
