50041efb26e13886511aef7507a094d79d6eae4b
iBlurBlur <tanakorn.ngam@gmail.com>
2020-01-28 10:17:29 +0700

    init header footer menu

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { LoginComponent } from './components/member/login/login.component';
 import { RegisterComponent } from './components/member/register/register.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
