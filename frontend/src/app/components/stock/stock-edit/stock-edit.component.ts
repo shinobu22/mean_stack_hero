3fcb423868567b1aca2cc03d1f78c6770e4bfe5e
iBlurBlur <tanakorn.ngam@gmail.com>
2020-01-28 17:15:44 +0700

    prepare stock edit

--- frontend/src/app/components/stock/stock-edit/stock-edit.component.ts
+++ frontend/src/app/components/stock/stock-edit/stock-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-stock-edit',
@@ -8,12 +10,39 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class StockEditComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  mProduct = new Product();
+  imageSrc: string | ArrayBuffer;
+
+  constructor(private activatedRoute: ActivatedRoute, private location: Location) {
+    this.mProduct.name = "";
+    this.mProduct.price = 0;
+    this.mProduct.stock = 0;
+  }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe( params => {
+    this.activatedRoute.params.subscribe(params => {
       alert(params.id)
     });
+  }
+
+  onSubmit() {
+    alert(this.mProduct.name);
+  }
+
+  onCancel() {
+    this.location.back();
+  }
+
+  onUpload(event) {
+    const metaImage = event.target.files[0];
+    if (metaImage) {
+      const reader = new FileReader();
+      reader.readAsDataURL(metaImage);
+      reader.onload = () => {
+        this.imageSrc = reader.result;
+        this.mProduct.image = metaImage;
+      };
+    }
   }
 
 }
